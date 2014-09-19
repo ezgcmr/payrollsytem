3a59d8f42f76befa81ff1102d8a71e9c2e45ac05
Ezgi Camur <ezgicamur@wpc-10-108-7-152.p.fiu.edu>
2014-09-19 18:31:54 -0400

    created secon construtor for employee class

--- src/Employee.java
+++ src/Employee.java
@@ -10,7 +10,7 @@ public class Employee {
 		private double accruedVacationHours;
 		private double yearToDate;
 		private ArrayList<Paycheck> listOfPaychecks;
-			
+	//first constructor		
 	public Employee(String employeeID, String firstName, String lastName,
 				String dateOfBirth, String dateHired,
 				double accruedVacationHours, double yearToDate,
@@ -26,79 +26,89 @@ public class Employee {
 			this.listOfPaychecks = listOfPaychecks;
 		}
 
-		public String getEmployeeID() {
-			return employeeID;
-		}
-		public void setEmployeeID(String employeeID) {
-			this.employeeID = employeeID;
-		}
-		public String getFirstName() {
-			return firstName;
-		}
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
-		public String getLastName() {
-			return lastName;
-		}
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
-		public String getDateOfBirth() {
-			return dateOfBirth;
-		}
-		public void setDateOfBirth(String dateOfBirth) {
-			this.dateOfBirth = dateOfBirth;
-		}
-		public String getDateHired() {
-			return dateHired;
-		}
-		public void setDateHired(String dateHired) {
-			this.dateHired = dateHired;
-		}
-		public double getAccruedVacationHours() {
-			return accruedVacationHours;
-		}
-		public void setAccruedVacationHours(double accruedVacationHours) {
-			this.accruedVacationHours = accruedVacationHours;
-		}
-		public double getYearToDate() {
-			return yearToDate;
-		}
-		public void setYearToDate(double yearToDate) {
-			this.yearToDate = yearToDate;
-		}
-		public ArrayList<Paycheck> getListOfPaychecks() {
-			return listOfPaychecks;
-		}
+	public Employee(String employeeID, String firstName, String lastName,
+			String dateOfBirth, String dateHired) {
+		super();
+		this.employeeID = employeeID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dateOfBirth = dateOfBirth;
+		this.dateHired = dateHired;
+	}
 
-		public void setListOfPaychecks(ArrayList<Paycheck> listOfPaychecks) {
-			this.listOfPaychecks = listOfPaychecks;
-		}
+	public String getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(String employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getDateHired() {
+		return dateHired;
+	}
+	public void setDateHired(String dateHired) {
+		this.dateHired = dateHired;
+	}
+	public double getAccruedVacationHours() {
+		return accruedVacationHours;
+	}
+	public void setAccruedVacationHours(double accruedVacationHours) {
+		this.accruedVacationHours = accruedVacationHours;
+	}
+	public double getYearToDate() {
+		return yearToDate;
+	}
+	public void setYearToDate(double yearToDate) {
+		this.yearToDate = yearToDate;
+	}
+	public ArrayList<Paycheck> getListOfPaychecks() {
+		return listOfPaychecks;
+	}
 
-		@Override
-		public String toString() {
-			return "Employee [employeeID=" + employeeID + ", firstName="
-					+ firstName + ", lastName=" + lastName + ", dateOfBirth="
-					+ dateOfBirth + ", dateHired=" + dateHired
-					+ ", accruedVacationHours=" + accruedVacationHours
-					+ ", yearToDate=" + yearToDate + "]";
-		}
-		
-	    public void addPaycheck(Paycheck check){
-	    	listOfPaychecks.add(check);
-	    }
+	public void setListOfPaychecks(ArrayList<Paycheck> listOfPaychecks) {
+		this.listOfPaychecks = listOfPaychecks;
+	}
 
-	    public double calculateGrossAmount(){
-	        return 0;
-	    }
+	@Override
+	public String toString() {
+		return "Employee [employeeID=" + employeeID + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", dateOfBirth="
+				+ dateOfBirth + ", dateHired=" + dateHired
+				+ ", accruedVacationHours=" + accruedVacationHours
+				+ ", yearToDate=" + yearToDate + "]";
+	}
+	
+    public void addPaycheck(Paycheck check){
+    	listOfPaychecks.add(check);
+    }
 
-	    public double calculateTaxDeduction(){
-	        return 0;
-	    }
+    public double calculateGrossAmount(){
+        return 0;
+    }
 
-	    public double calculateVactionHours(){
-	        return 0;
-	    }
+    public double calculateTaxDeduction(){
+        return 0;
+    }
+
+    public double calculateVactionHours(){
+        return 0;
+    }
 		
 }
